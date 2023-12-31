a0e45e5815492df8dcdfb8b4771aaffca4e1cc64
Sonja Gustafson <124421321+sgustafson123@users.noreply.github.com>
2023-12-31 02:53:21 -0600

    docker

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f8f41093e4550420d982862c85a6a456'),
+    },
+  },
+});
